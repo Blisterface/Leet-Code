36c86d48f5763bffb12f8f19ca1bad4037188e2e
Joseph Bliss <josephblister@gmail.com>
2020-10-07 02:12:43 +0200

    optimised max consecutive ones to O(n)

--- max_consecutive_ones.cpp
+++ max_consecutive_ones.cpp
@@ -4,11 +4,11 @@ using namespace std;
 
 int max_cons_ones(vector<int>& v)
 {
-    int j=0,res=0;
-    for(int i =0;i<v.size();i++)
+    int j=0,res=0,i=0,size=v.size();
+    while(i<size)
     {
         int temp = 0;
-        while(j<v.size())
+        while(j<size)
         {
             if(v[j]==1)
             {
@@ -20,16 +20,34 @@ int max_cons_ones(vector<int>& v)
                 j++;
                 break;
             }
-            
         }
+        i=j;
         res = max(res,temp);
     }
     return res;
 }
 
+int sol_two(vector<int> nums)
+{
+    int res=0,size=nums.size(),temp=0;
+    for(int i=0;i<size;i++)
+    {
+        if(nums[i]!=1){
+            temp = 0;
+            continue;
+        }
+        else
+        {
+            temp+=nums[i];
+            res=max(res,temp);
+        }
+        
+    }
+    return res;
+}
 int main()
 {
     vector<int> vec{1,1,0,1,1,1};
-    cout<<max_cons_ones(vec)<<endl;
+    cout<<sol_two(vec)<<endl;
     return 0;
 }
