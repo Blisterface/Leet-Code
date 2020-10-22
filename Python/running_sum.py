efabc57e77b2e91479a7bb4efaf13650569caa19
Blisterface <josephblister@gamil.com>
2020-10-22 22:04:26 +0200

    cleaned up running sum

--- Python/running_sum.py
+++ Python/running_sum.py
@@ -1,11 +1,9 @@
 from typing import List
 
-print([1,4,6,8,12])
 def running_sum(nums: List[int])->List[int]:
     i = 1
     for i in range(1,len(nums)):
         nums[i] = nums[i] + nums[i-1]
-
     return nums
 
 if __name__ == '__main__':
